1efb66d2d19197bfa8a898b31394205378a9d19c
fbugcn <fbug@tom.com>
2019-11-24 19:45:03 +0800

    mod dockerfile

--- FBug.Deploying.WebAPI/Controllers/ValuesController.cs
+++ FBug.Deploying.WebAPI/Controllers/ValuesController.cs
@@ -21,25 +21,7 @@ namespace FBug.Deploying.WebAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return string.Concat("value", id);
         }
     }
 }
